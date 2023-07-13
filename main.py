4d4d057ca583b4d4828bbcea71c3fb538fbcafb6
adelkovalevkzn <github@adelkovalevkzn.ru>
2023-07-13 13:21:01 +0300

    Классы разделены по разным скриптам

--- main.py
+++ main.py
@@ -7,37 +7,47 @@ from parser import Parser
 from errors import UnknownMethodError
 
 
+def run(filename, method, params, output):
+    aug = Augmentation(filename)
+
+    if method == 'resize':
+        params = tuple(map(int, (params[0], params[1])))
+        aug.resize(params)
+
+    elif method == 'shift':
+        aug.shift(int(params[0]), int(params[1]))
+
+    elif method == 'crop':
+        x = tuple(map(int, (params[0], params[1])))
+        y = tuple(map(int, (params[2], params[3])))
+        aug.crop(x, y)
+
+    elif method == 'rotate':
+        angle = int(params[0])
+        scale = float(params[1])
+        aug.rotate(angle, scale)
+
+    else:
+        raise UnknownMethodError
+
+    aug.save_image(output)
+
+
 def main():
     parser = Parser().get_parser()
     args = parser.parse_args()
 
     filename = args.filename
     method = args.method
+    params = args.params
     output = args.output
 
-    aug = Augmentation(args.filename)
+    if os.path.isfile(filename):
+        run(filename, method, params, output)
 
-    if method == 'resize':
-        params = tuple(map(int, (args.params[0], args.params[1])))
-        aug.resize(params)
-
-    elif method == 'shift':
-        aug.shift(int(args.params[0]), int(args.params[1]))
-
-    elif method == 'crop':
-        x = tuple(map(int, (args.params[0], args.params[1])))
-        y = tuple(map(int, (args.params[2], args.params[3])))
-        aug.crop(x, y)
-
-    elif method == 'rotate':
-        angle = int(args.params[0])
-        scale = float(args.params[1])
-        aug.rotate(angle, scale)
-
-    else:
-        raise UnknownMethodError
-
-    aug.save_image(args.output)
+    elif os.path.isdir(filename):
+        files = os.listdir(filename)
+        print(files)
 
 
 if __name__ == '__main__':
